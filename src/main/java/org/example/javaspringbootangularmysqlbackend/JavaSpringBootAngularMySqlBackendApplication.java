76e5854ec22098f20cc55ea8676ed84330f99f3d
ranuthd <ranuthd@codegen.net>
2024-07-20 12:02:06 +0530

    First Commit - Default Things

---
+++ src/main/java/org/example/javaspringbootangularmysqlbackend/JavaSpringBootAngularMySqlBackendApplication.java
@@ -0,0 +1,15 @@
+package org.example.javaspringbootangularmysqlbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootAngularMySqlBackendApplication
+{
+
+    public static void main( String[] args )
+    {
+        SpringApplication.run( JavaSpringBootAngularMySqlBackendApplication.class, args );
+    }
+
+}
---
+++ src/test/java/org/example/javaspringbootangularmysqlbackend/JavaSpringBootAngularMySqlBackendApplicationTests.java
@@ -0,0 +1,15 @@
+package org.example.javaspringbootangularmysqlbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBootAngularMySqlBackendApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
